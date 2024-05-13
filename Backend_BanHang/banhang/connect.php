fb64bc672135bc65cd28abe0d35e00e2976220eb
NgocAn23 <144838519+NgocAn23@users.noreply.github.com>
2024-05-13 16:24:49 +0700

    NgocAn commit

---
+++ Backend_BanHang/banhang/connect.php
@@ -0,0 +1,9 @@
+﻿<?php 
+$host = "localhost";
+$user = "root";
+$pass = "";
+$database = "dataonline";
+
+$conn = mysqli_connect($host, $user , $pass , $database);
+mysqli_set_charset($conn, "utf8");
+?>
